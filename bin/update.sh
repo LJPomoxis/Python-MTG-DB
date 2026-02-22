7a9332ce74537ae352454d7faf4c1af621aefafc
LJPomoxis <lukeottojack@gmail.com>
2026-02-21 20:35:24 -0700

    Bump version: 0.1.7 → 0.1.8 - Altered update script with colors, nice.

--- bin/update.sh
+++ bin/update.sh
@@ -2,60 +2,71 @@
 
 VENV_BIN="/var/www/mtgwebapp/venv/bin"
 
-echo "Staging changes..."
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+GRAY='\033[1;30m'
+YELLOW='\033[1;33m'
+LBLUE='\033[1;34m'
+LCYAN='\033[1;36m'
+NC='\033[0m'
+
+printf "Staging changes...\n"
 git add /var/www/mtgwebapp/
 
-# Requires that bump-my-version is installed and .bumpversion.cfg exists
+# Requires that bump-my-version is installed and .bumpversion.cfg or pyproject.toml exists
 
 OPTION="invalid"
 
 while [ "$OPTION" == "invalid" ]; do
-    echo "What kind of update is this?"
-    echo "1) Patch (Bug fix)"
-    echo "2) Minor (New feature)"
-    echo "3) Major (Breaking change)"
-    read -p "Selection [1-3]: " choice
+    printf "${LCYAN}Select Update Type${NC}\n"
+    printf "1) ${GRAY}Patch${NC} (Bug fix)\n"
+    printf "2) ${BLUE}Minor${NC} (New feature)\n"
+    printf "3) ${LBLUE}Major${NC} (Breaking change)\n"
+    printf "${LCYAN}Selection${NC} [${GRAY}1-3${NC}]: "
+    read choice
 
     case $choice in
         1) OPTION="patch" ;;
         2) OPTION="minor" ;;
         3) OPTION="major" ;;
-        *) OPTION="invalid"; echo -e "\n[!] Invalid selection. Please try again.\n" ;;
+        *) OPTION="invalid"; printf "\n${YELLOW}[!] Invalid selection.${NC} Please try again.\n" ;;
     esac
 done
 
-echo ""
-read -p "Enter commit description: " commitvar
+printf "\n"
+printf "${LCYAN}Enter commit description${NC}: "
+read commitvar
 
 while [ -z "$commitvar" ]; do
-    echo "[!] Commit message cannot be empty!"
-    read -p "Enter commit description: " commitvar
+    printf "${YELLOW}[!] Commit message cannot be empty!${NC}\n"
+    printf "${LCYAN}Enter commit description${NC}: "
+    read commitvar
 done
 
 echo "---------------------------------------"
 if [ "$OPTION" == "patch" ]; then
-    echo "Patching and comitting..."
+    printf "${GREEN}Performing patch and comitting...${NC}\n"
 else
-    echo "$OPTION update and committing..."
+    printf "${GREEN}Performing $OPTION update and committing...${NC}\n"
 fi
-
-
 
 $VENV_BIN/bump-my-version bump $OPTION \
     --allow-dirty \
     --commit --tag --message "Bump version: {current_version} → {new_version} - $commitvar"
 
 if [ $? -eq 0 ]; then
-    echo "---------------------------------------"
-    echo "Success! Version updated and tagged."
+    printf "---------------------------------------\n"
+    printf "${GREEN}Success! Version updated and tagged.${NC}\n"
 else
-    echo "---------------------------------------"
-    echo "Error: Something went wrong with the version bump."
+    printf "---------------------------------------\n"
+    printf "${RED}Error: Something went wrong with the version bump.${NC}\n"
 fi
 
-read -p "Push to origin? (y/n): " pushchoice
-if [ "$pushchoice" == "y" ]; then
+printf "Push to origin? (${GREEN}y${NC}/${RED}n${NC}): "
+read pushvar
+if [ "$pushvar" == "y" ]; then
     git push origin main --tags
 else
-    echo "Ensure code is pushed to origin at a later time."
+    printf "${YELLOW}Ensure code is pushed to origin at a later time.${NC}\n"
 fi
--- mtgwebapp.py
+++ mtgwebapp.py
@@ -26,7 +26,7 @@ if __name__ != '__main__':
 
 load_dotenv()
 
-MTG_APP_VERSION = "0.1.7"
+MTG_APP_VERSION = "0.1.8"
 
 EMAIL = os.getenv('EMAIL')
 APP_INFO = F"mtgDB/{MTG_APP_VERSION} ({EMAIL})"
